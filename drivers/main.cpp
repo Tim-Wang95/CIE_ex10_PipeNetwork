03d043626700b5a52df3be023bc853ca457b60f9
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2019-01-16 04:24:58 +0800

    Update main.cpp

--- drivers/main.cpp
+++ drivers/main.cpp
@@ -2,16 +2,29 @@
 
 int main() {
 
-	std::string filename = "C:/Users/tan84/Desktop/input.txt";
-	cie::pipenetwork::PipeNetwork p(filename);
+	std::string infilename = "C:/Users/tan84/Desktop/input.txt";
+	std::string outfilename = "C:/Users/tan84/Desktop/pipedata.txt";
 
-	//p.print();
-
+	// calculate fluxes
+	cie::pipenetwork::PipeNetwork p(infilename);
 	std::vector<double> q = p.computeFluxes();
-
+	
+	// output file
+	std::ofstream outfile; 
+	outfile.open(outfilename);
 	for (int i = 0; i < q.size(); i++) {
-		std::cout << i+1 << ".  " << q[i] << " " << std::endl;
+		outfile << i+1 << ".  " << q[i] << " " << std::endl;
 	}
+	outfile.close();
+
+	// read file back in
+	std::string line;
+	std::ifstream read(outfilename);
+	while (std::getline(read, line)) {
+		std::cout << line << std::endl;
+	}
+	read.close();
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 
