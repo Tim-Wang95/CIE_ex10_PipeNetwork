016bfe6f3e9a812a39a282ca7d256cbbf51da1dc
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2019-01-16 04:39:06 +0800

    Update additional.cpp

--- library/test/additional.cpp
+++ library/test/additional.cpp
@@ -22,17 +22,13 @@ namespace cie {
 		TEST_CASE("Tube_test_additional") {
 			Node node1(1.1, 0.9, 19, 95);
 			Node node2(4.1, 4.9, 0, 96);
+			
+			double tolerance = 1e-14; 
+			double permeability = 3.14159265358979323846 * 9.81 * std::pow(0.3, 4) / (128 * 1e-6 * 5);
 
 			Tube tube(&node1, &node2, 0.3);
 
-			double tolerance = 1e-14;
-
 			CHECK(tube.length() == Approx(5).epsilon(tolerance));
-
-			double pi = 3.14159265358979323846;
-
-			double permeability = pi * 9.81 * std::pow(0.3, 4) / (128 * 1e-6 * 5);
-
 			CHECK(tube.permeability() == Approx(permeability).epsilon(tolerance));
 		}
 
